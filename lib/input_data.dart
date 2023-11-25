a457524332b2385dfb04b103cb374303dae610c3
NaufalParamaRafif <profesoraiueo@gmail.com>
2023-11-25 11:25:55 +0700

    add master barang

--- lib/input_data.dart
+++ lib/input_data.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -14,7 +15,10 @@ class InputData extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text("Cart", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+        title: Text(
+          "Cart",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
         leading: Container(
           margin: const EdgeInsets.all(5.0),
@@ -22,13 +26,13 @@ class InputData extends StatelessWidget {
             color: Colors.white,
             borderRadius: BorderRadius.circular(25.0),
             boxShadow: [
-                BoxShadow(
-                  color: const Color.fromARGB(255, 92, 92, 92).withOpacity(0.5),
-                  spreadRadius: 1,
-                  blurRadius: 1,
-                  offset: Offset(0, 3), // changes position of shadow
-                ),
-              ],
+              BoxShadow(
+                color: const Color.fromARGB(255, 92, 92, 92).withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 1,
+                offset: Offset(0, 3),
+              ),
+            ],
           ),
           child: IconButton(
             onPressed: () {},
@@ -40,13 +44,14 @@ class InputData extends StatelessWidget {
             margin: const EdgeInsets.all(5.0),
             decoration: BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.circular(25.0), // Adjust the radius as needed
+              borderRadius:
+                  BorderRadius.circular(25.0), // Adjust the radius as needed
               boxShadow: [
                 BoxShadow(
                   color: const Color.fromARGB(255, 92, 92, 92).withOpacity(0.5),
                   spreadRadius: 1,
                   blurRadius: 1,
-                  offset: Offset(0, 3), // changes position of shadow
+                  offset: Offset(0, 3),
                 ),
               ],
             ),
@@ -59,7 +64,178 @@ class InputData extends StatelessWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(/*...*/),
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 20,
+        ),
+        color: const Color.fromARGB(255, 174, 204, 255),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: 500,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      height: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            blurRadius: 5,
+                            spreadRadius: 1,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          InkWell(
+                            onTap: () {},
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(top: 10, right: 60),
+                                  height: 90,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                Image.asset(
+                                  "images/",
+                                  height: 130,
+                                  width: 130,
+                                  fit: BoxFit.contain,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(vertical: 30),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Burgir",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 23,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(5),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(10),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color:
+                                                Colors.black.withOpacity(0.3),
+                                            blurRadius: 5,
+                                            spreadRadius: 1,
+                                          ),
+                                        ],
+                                      ),
+                                      child: Icon(
+                                        CupertinoIcons.minus,
+                                        size: 18,
+                                      ),
+                                    ),
+                                    Container(
+                                      margin:
+                                          EdgeInsets.symmetric(horizontal: 10),
+                                      child: Text(
+                                        "5",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(5),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(10),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color:
+                                                Colors.black.withOpacity(0.3),
+                                            blurRadius: 5,
+                                            spreadRadius: 1,
+                                          ),
+                                        ],
+                                      ),
+                                      child: Icon(
+                                        CupertinoIcons.add,
+                                        size: 18,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          Spacer(),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 25,
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.3),
+                                        blurRadius: 5,
+                                        spreadRadius: 1,
+                                      ),
+                                    ],
+                                  ),
+                                  child: Icon(
+                                    Icons.delete,
+                                    color: Colors.red,
+                                    size: 20,
+                                  ),
+                                ),
+                                Spacer(),
+                                Text(
+                                  "\$50",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,53 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Container(
+          margin: const EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(25.0),
+            boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(255, 92, 92, 92).withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+          ),
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.list, color: Colors.orange),
+          ),
+        ),
+        actions: [
+          Container(
+            margin: const EdgeInsets.all(5.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25.0), // Adjust the radius as needed
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(255, 92, 92, 92).withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: IconButton(
+              onPressed: () => {},
+              icon: Icon(
+                Icons.person,
+                color: Colors.orange,
+            )),
+          ),
+        ],
+      ),
       body: Center(
         child: Text(
           "Center",
